5bc445e2ae13edc9065818ba079e3e4ac97f34d2
akshay-joseph-jimmy <akshayjosephjimmy05@gmail.com>
2026-04-23 10:40:53 +0530

    commit

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { SignUp } from "@clerk/nextjs";
+
+
+function SignUpPage(){
+
+
+    return(
+        <div className="flex items-center justify-center h-screen">
+            <SignUp/>
+        </div>
+    )
+}
+export default SignUpPage;
---
+++ app/(dashboard)/projects/page.tsx
@@ -0,0 +1,12 @@
+
+
+function ProjectsPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Projects</h1>
+      <p>List of projects will go here.</p>
+    </div>
+  );
+}
+
+export default ProjectsPage;
